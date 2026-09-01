737bde682f304863e7012ac19e44604d65efc5f6
Yash Jaiswal <yashjaiswal8899@gmail.com>
2026-09-01 23:54:07 +0530

    posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404,render,redirect
+from django.http import HttpResponse
+from .models import Blog, Category
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    # use get_object_or_404 whaen you want to show 4040
+    context = {
+        'posts': posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html',context)
